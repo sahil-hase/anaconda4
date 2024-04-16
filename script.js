ea39378bb0109667db071a9c64f47dfd65ee7766
sahil-hase <162993831+sahil-hase@users.noreply.github.com>
2024-04-16 15:42:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+var menubutton = document.querySelector('.Sahil');
+var Nav2 = document.querySelector('.nav2');
+
+
+menubutton.addEventListener("click", function() {
+    Nav2.classList.toggle('dropdown');
+})
